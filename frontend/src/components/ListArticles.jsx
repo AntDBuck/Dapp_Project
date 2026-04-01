68fb44bb7176b8a7a7be3f3f4f85223b6d122b80
AntDBuck <antonybuck@outlook.com>
2026-04-01 11:54:58 +0100

    List articles for use in articles page

--- frontend/src/components/ListArticles.jsx
+++ frontend/src/components/ListArticles.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { formatTime, formatAddress } from "./UtilTools";
 
-function ListArticles() 
+function ListArticles({ articles }) 
 {
-    const [articles, setArticles] = useState([]);
-    const [errorMsg, setErrorMsg] = useState('');
-    const [successMsg, setSuccessMsg] = useState('');
-
+    return (
+        <section>
+            {
+                articles.length === 0 ? 
+                (
+                    <h3>No articles found.</h3>
+                ) :
+                (
+                    articles.map((article) =>
+                        (
+                            <div key={article.articleId}>
+                                <h4>{article.title}</h4>
+                                <p>Author: {formatAddress(article.author)}</p>
+                                <p>Published on: {formatTime(article.publishedTime)}</p>
+                                <p>Last updated: {formatTime(article.updatedTime)}</p>
+                                <p>CID: {article.cid}</p>
+                            </div>
+                        )
+                    )
+                )
+            }
+        </section>
+    );
 };
 
 export default ListArticles;
