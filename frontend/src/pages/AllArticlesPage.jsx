a07dcc9f49897c1f91cba2a9bd5cfd45cab05b4a
AntDBuck <antonybuck@outlook.com>
2026-04-03 16:30:38 +0100

    Complete overhaul of viewing articles.

--- frontend/src/pages/AllArticlesPage.jsx
+++ frontend/src/pages/AllArticlesPage.jsx
@@ -1,66 +1,196 @@
 import { useState, useEffect } from "react";
-import Title from "../components/Title";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import IsLoading from "../components/IsLoading";
 import ListArticles from "../components/ListArticles";
-import { Container, Row, Col } from "react-bootstrap";
+import ViewArticle from "../components/ViewArticle";
 
-function AllArticlesPage({ contract, account })
+function AllArticlesPage({ contract, account }) 
 {
     const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [status, setStatus] = useState('loading');
+    const [filteredArticles, setFilteredArticles] = useState('all');
+    const [selectedArticleBody, setSelectedArticleBody] = useState(null);
+    const [selectedArticleMeta, setSelectedArticleMeta] = useState(null);
 
     useEffect(() => 
     {
-        const loadArticles = async () =>
+        const loadArticles = async () => 
         {
-            if (!contract) return;
+            if (!contract || !account ) return;
 
-            try 
+            setStatus('loading');
+
+            try
             {
-                const allArticles = await contract.methods.getAllArticles().call({ from: account });
+                let fetchedArticles = [];
 
-                const formattedArticles = allArticles.map((article) => 
-                (    
+                if (filteredArticles === 'all')
+                {
+                    const allArticles = await contract.methods.getAllArticles().call({ from: account });
+
+                    fetchedArticles = allArticles.filter((article) => !article.deleted).map((article => 
+                    (
+                        {
+                            articleId: article.articleId,
+                            cid: article.cid,
+                            title: article.title,
+                            publishedTime: article.publishedTime,
+                            updatedTime: article.updatedTime,
+                            author: article.author
+                        }
+                    )))
+                }
+                else if (filteredArticles === 'mine')
+                {
+                    const myArticleCount = await contract.methods.getMyArticleCount().call({ from: account });
+                    
+                    for (let i = 0; i < myArticleCount; i++)
                     {
-                        articleId: article.articleId,
-                        cid: article.cid,
-                        title: article.title,
-                        publishedTime: article.publishedTime,
-                        updatedTime: article.updatedTime,
-                        author: article.author
-                    }
-                ));
+                        const article = await contract.methods.getMyArticle(i).call({ from: account });
 
-                setArticles(formattedArticles);
-                setLoading(false);
+                        if (!article.deleted)
+                        {
+                            fetchedArticles.push(
+                                {
+                                    articleId: article.articleId,
+                                    cid: article.cid,
+                                    title: article.title,
+                                    publishedTime: article.publishedTime,
+                                    updatedTime: article.updatedTime,
+                                    author: article.author
+                                }
+                            );
+                        }
+                    }
+                }
+                setArticles(fetchedArticles);
             }
             catch (err) 
             {
                 console.error('Could not get articles:', err);
-                setLoading(false);
-            };
+            }
+            finally
+            {
+                setStatus('ready');
+            }
         };
 
         loadArticles();
         console.log(contract)
-    }, [contract, account]
+    }, [contract, account, filteredArticles]
     );
+
+    const articleClick = async (cid) => 
+    {
+        setStatus('fetching');
+        try 
+        {
+            const res = await fetch(`https://gateway.pinata.cloud/ipfs/${cid}`);
+            const articleData = await res.json();
+            setSelectedArticleBody(articleData);
+
+            const metaData = articles.find((article) => article.cid === cid);
+            setSelectedArticleMeta(metaData)
+        }
+        catch (err) {
+            console.log('Article could not be loaded from Pinata gateway:', err);
+            alert('Error, could not load article!');
+        }
+        finally
+        {
+            setStatus('ready');
+        }
+    };
+
+    const deleteArticle = async (articleId) =>
+    {
+        setStatus('deleting');
+        try
+        {
+            await contract.methods.deleteArticle(articleId).send({ from: account });
+
+            setArticles((prevArticles) => 
+            (
+                prevArticles.filter((article) =>
+                (
+                    article.articleId !== articleId
+                ))
+            ));
+            setSelectedArticleBody(null);
+            setSelectedArticleMeta(null);
+        }
+        catch (err)
+        {
+            console.error('Error deleting article:', err);
+        }
+        finally
+        {
+            setStatus('ready');
+        }
+    };
 
     return (
         <div>
-            <Title titleString='All Articles' />
-            {loading ? <IsLoading /> : 
-                <Container>
-                    <Row>
-                        <Col md={3}>
-                            <p>Sorting and filtering component goes here</p>
-                        </Col>
-                        <Col md={6}>
-                            <ListArticles articles={articles} />
-                        </Col>
-                    </Row>
-                </Container>
-            }
+            <Container fluid>
+                <Row>
+                    <Col md={2} className='d-flex flex-column align-items-center mt-5 p-4 gap-5 side-bar'>
+                        <Button
+                            variant='dark'
+                            size='lg'
+                            disabled={status !== 'ready'}
+                            onClick={() => 
+                                {
+                                    setFilteredArticles('all'),
+                                    setSelectedArticleBody(null),
+                                    setSelectedArticleMeta(null)
+                                }
+                            }
+                        >
+                            All Articles
+                        </Button>
+                        <Button
+                            variant='dark'
+                            size='lg'
+                            disabled={status !== 'ready'}
+                            onClick={() => 
+                                {
+                                    setFilteredArticles('mine'),
+                                    setSelectedArticleBody(null),
+                                    setSelectedArticleMeta(null)
+                                }
+                            }
+                        >
+                            My Articles
+                        </Button>
+                    </Col> 
+                    <Col md={10}>
+                        <h1 className='text-center pb-2'>
+                            {filteredArticles === 'all' ? 'All Articles' : 'My Articles'}
+                        </h1>
+                        {status === 'loading' && <IsLoading msg='Loading Articles...' />}
+                        {status === 'deleting' && <IsLoading msg='Deleting Article...' />}
+                        {status === 'fetching' && <IsLoading msg='Fetching Article...' />}
+                        {
+                            status === 'ready' && 
+                            <>
+                                {
+                                    selectedArticleBody && selectedArticleMeta ? 
+                                    <ViewArticle 
+                                        articleBody={selectedArticleBody} 
+                                        articleMetaData={selectedArticleMeta}
+                                        account={account}
+                                        onDelete={deleteArticle}
+                                    /> :
+                                    <ListArticles 
+                                        articles={articles}
+                                        onArticleClick={articleClick} 
+                                    />
+                                }
+                            </>
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
