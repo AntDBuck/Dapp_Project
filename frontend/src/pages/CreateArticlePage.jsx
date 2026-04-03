4dd006f44e54587db9e2a1134fc6dd0cec7f70bb
AntDBuck <antonybuck@outlook.com>
2026-04-03 16:29:45 +0100

    Added various improvements including image compression, updateContent, and status loading.

--- frontend/src/pages/CreateArticlePage.jsx
+++ frontend/src/pages/CreateArticlePage.jsx
@@ -1,10 +1,11 @@
 import { useDraggable, useDroppable, DragDropProvider } from '@dnd-kit/react';
 import { useState } from 'react';
-import { generateId } from '../components/UtilTools';
 import { Row, Col, Button } from 'react-bootstrap';
-import '../styles/createArticle.css'
+import { generateId, imgCompressAndBase64Convert, titleValidator } from '../components/UtilTools';
+import IsLoading from '../components/IsLoading';
 
-function DragBlock({ blockType, disabled }) {
+function DragBlock({ blockType, disabled }) 
+{
     const { ref } = useDraggable({ id: blockType, disabled });
 
     return ( 
@@ -14,7 +15,8 @@ function DragBlock({ blockType, disabled }) {
     );
 };
 
-function DropBox({ id, children }) {
+function DropBox({ id, children }) 
+{
     const { ref } = useDroppable({ id });
 
     return (
@@ -24,140 +26,199 @@ function DropBox({ id, children }) {
     );
 };
 
-function RenderBlock({ block, onContentChange }) {
-    const imageChangeHandler = (e) => {
+function RenderBlock({ block, onContentChange }) 
+{
+    const imageChange = async (e) => 
+    {
         const imgFile = e.target.files[0];
         if (!imgFile) return;
 
-        const reader = new FileReader();
-        reader.onload = () => onContentChange(reader.result);
-
-        reader.readAsDataURL(imgFile);
+        const base64Img = await imgCompressAndBase64Convert(imgFile);
+        onContentChange(base64Img);
     }
 
     if (!block) return null;
 
-    switch (block.blockType) {
-        case 'Heading':
-            return <input 
-                        type='text' 
-                        value={block.blockContent || ''} 
-                        placeholder='Heading...' 
-                        onChange={(e) => onContentChange(e.target.value)}
-                        className='inputs input-headings'
-                    />
+    switch (block.blockType) 
+    {
         case 'Sub-Heading':
             return <input 
                         type='text' 
                         value={block.blockContent || ''} 
-                        placeholder='Sub-Heading...' 
+                        placeholder='Sub-Heading...'
+                        maxLength={50}
                         onChange={(e) => onContentChange(e.target.value)}
                         className='inputs input-headings'
                     />
         case 'Text':
-            return <textarea 
+            return <textarea
                         value={block.blockContent || ''} 
-                        placeholder='Text...' 
-                        onChange={(e) => onContentChange(e.target.value)}
+                        placeholder='Text...'
+                        maxLength={1000}
+                        onChange={(e) => 
+                        {
+                            e.target.style.height = 'auto';
+                            e.target.style.height = e.target.scrollHeight + 'px'
+                            onContentChange(e.target.value)
+                        }}
                         className='inputs'
                     />
         case 'Image': 
             return ( 
-                <div className='d-flex flex-column'>
+                <div className='d-flex flex-column align-items-center w-100 mx-5 my-1'>
                     {block.blockContent ? <img src={block.blockContent} className='inputs-img'/> : null}
-                    <input type='file' accept='image/*' onChange={imageChangeHandler} />
+                    <input type='file' accept='image/*' onChange={imageChange} />
                 </div>
             )
         default:
             return null;
     }
-}
+};
 
-function CreateArticlePage({ account, contract }) {
+function CreateArticlePage({ account, contract }) 
+{
+    const [ status, setStatus ] = useState(false);
     const [ articleTitle, setArticleTitle ] = useState('');
 
-    const [ slots, setSlots ] = useState([
-        {id: generateId(), slotWidth: 'full', block: null},
-    ]);
+    const [ rows, setRows ] = useState(
+        [
+            {id: generateId(), columns : [{ id: generateId(), block: null }]}
+        ]
+    );
+    
+    const imgCount = rows
+        .flatMap((row) => row.columns)
+        .filter((col) => col.block?.blockType === 'Image').length;
 
-    const imgCount = slots.filter((slot) => slot.block?.blockType === 'Image').length;
-
-    const addBlockHandler = (source, target) => {
-        setSlots((prevState) => (
-            prevState.map((slot) => {
-                if (slot.id === target.id) {
-                    return {...slot, 
-                        block: { 
-                            blockId: generateId(), 
-                            blockType: source.id, 
-                            blockContent: '' 
-                        }
-                    };
+    const addBlock = (source, target) => 
+    (
+        setRows((prevRows) => 
+        (
+            prevRows.map((row) => 
+            (
+                {
+                    ...row,
+                    columns: row.columns.map((col) => 
+                    (
+                        col.id === target.id ? 
+                        {
+                            ...col, 
+                            block: {blockId: generateId(), blockType: source.id, blockContent: ''}
+                        } 
+                        : col
+                    ))
                 }
-                return slot;
-            }))
-        )
-    };
-
-    const addFullSlotHandler = () => (
-        setSlots((prevState) => [
-            ...prevState, 
-            {id: generateId(), slotWidth: 'full', block: null}
-        ])
-    );
-
-    const addHalfSlotsHandler = () => (
-        setSlots((prevState) => [
-            ...prevState, 
-            {id: generateId(), slotWidth: 'half', block: null},
-            {id: generateId(), slotWidth: 'half', block: null}
-        ])
-    );
-
-    const removeSlotHandler = (slotId) => (
-        setSlots((prevState) => (
-            prevState.filter(slot => slot.id !== slotId)
-        ))
-    );
-
-    const updateContentHandler = (slotId, newContent) => {
-        setSlots((prevState) => (
-            prevState.map((slot) => (
-                slot.id === slotId ? {...slot, block: {...slot.block, blockContent: newContent}} : slot
             ))
         ))
-    };
+    );
 
-    const publishArticleHandler = async () => {
-        try {
-            if (!account || !contract) {
+    const addFullRow = () => 
+    (
+        setRows((prevRows) => 
+        [
+            ...prevRows, 
+            {
+                id: generateId(), 
+                columns: [{ id: generateId(), block: null }]
+            }
+        ])
+    );
+
+    const addHalfRow = () => 
+    (
+        setRows((prevRows) => 
+        [
+            ...prevRows,
+            {
+                id: generateId(),
+                columns: 
+                [
+                    {id: generateId(), block: null},
+                    {id: generateId(), block: null}
+                ]
+            }
+        ])
+    );
+
+    const removeRow = (rowId) => 
+    (   
+        setRows((prevRows) => 
+        (
+            prevRows.filter((row) => row.id !== rowId)
+        ))
+    );
+
+    const updateContent = (colId, newContent) => 
+    (
+        setRows((prevRows) => 
+        (
+            prevRows.map((row) => 
+            (
+                {
+                    ...row,
+                    columns: row.columns.map((col) => 
+                    (
+                        col.id === colId ? 
+                        {
+                            ...col, 
+                            block: {...col.block, blockContent: newContent}
+                        }
+                        : col
+                    )) 
+                }
+            ))
+        ))
+    );
+
+    const publishArticle = async () => 
+    {
+        setStatus(true);
+        try 
+        {
+            if (!account || !contract) 
+            {
                 alert('Account or contract not loaded. Please try again.');
                 return;
             }
 
-            if (!articleTitle.trim()) {
+            if (!articleTitle.trim()) 
+            {
                 alert('Article title empty. Please add a title.');
                 return;
             }
 
-            const articleData = {
+            const articleData = 
+            {
                 title: articleTitle,
-                blocks: slots.map((slot) => ({
-                    blockType: slot.block?.blockType || null,
-                    blockContent: slot.block?.blockContent || null
-                }))
+                rows: rows.map((row) => 
+                (
+                    {
+                        columns: row.columns.map((col) => 
+                        (
+                            {
+                                block: col.block ? 
+                                {
+                                    blockType: col.block.blockType,
+                                    blockContent: col.block.blockContent
+                                }
+                                : null
+                            }
+                        ))
+                    }
+                ))
             }
 
-            const response = await fetch('http://localhost:5000/upload-json-data', {
+            const response = await fetch('http://localhost:5000/upload-json-data', 
+            {
                 method: 'POST',
-                body: new Blob([JSON.stringify(articleData)],
-                    { type: 'application/json'} 
-                )
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(articleData)
             });
 
             const responsedData = await response.json();
 
-            if (!responsedData.cid) {
+            if (!responsedData.cid) 
+            {
                 alert('falied to upload article!');
                 return;
             }
@@ -165,89 +226,112 @@ function CreateArticlePage({ account, contract }) {
             const cid = responsedData.cid;
             console.log('Article uploaded:', cid);
 
-            await contract.methods.publishArticle(cid, articleTitle)
-                .send({ from: account }
-            );
+            await contract.methods.publishArticle(cid, articleTitle).send({ from: account });
         }
-        catch (err) {
+        catch (err) 
+        {
             console.error('Error, could not publish article:', err);
+        }
+        finally
+        {
+            setStatus(false);
         }
     };
 
     return (
         <DragDropProvider
-            onDragEnd={(e) => { 
+            onDragEnd={(e) => 
+            { 
                 if (e.canceled) return;
 
                 const { source, target } = e.operation;
 
                 if (!target) return;
-
-                if (target) addBlockHandler(source, target);
+                addBlock(source, target);
             }}
         >
             <Row>
                 <Col md={2} className='d-flex flex-column align-items-center p-4 gap-3 side-bar'>
-                    <DragBlock blockType='Heading' />
-                    <DragBlock blockType='Sub-Heading' />
-                    <DragBlock blockType='Text' />
-                    <DragBlock blockType='Image' disabled={imgCount >= 4} />
-                    {imgCount >= 4 && (
-                        <small className='text-center text-danger'>
-                            A limit of 4 images per article
-                        </small>
-                    )}
+                    <DragBlock blockType='Sub-Heading' disabled={status} />
+                    <DragBlock blockType='Text' disabled={status} />
+                    <DragBlock blockType='Image' disabled={imgCount >= 4 || status} />
+                    {
+                        imgCount >= 4 && 
+                        (
+                            <small className='text-center text-danger'>
+                                A limit of 4 images per article
+                            </small>
+                        )
+                    }
                 </Col>
                 <Col md={10} className='p-5 border'>
-                    <Row>
-                        {slots.map((slot) => (
-                            <Col 
-                                key={slot.id}
-                                md={slot.slotWidth === 'full' ? 12 : 6}
-                                className='mb-3'
-                            >
-                                <DropBox id={slot.id}>
-                                    <RenderBlock
-                                        block={slot.block} 
-                                        onContentChange={(value) => updateContentHandler(slot.id, value)}
+                    {
+                        status ? <IsLoading msg='Publishing Article...' /> :
+                        <>
+                            <Row>
+                                <div className='d-flex justify-content-center mb-4'>
+                                    <input
+                                        type='text'
+                                        value={articleTitle} 
+                                        onChange={(e) => setArticleTitle(titleValidator(e.target.value))}
+                                        placeholder='Article Title'
+                                        maxLength={30}
+                                        className='form-control mx-5 py-2 input-title'
                                     />
-                                </DropBox>
-                                <div className='text-center mt-2'>
-                                    <Button
-                                        onClick={() => removeSlotHandler(slot.id)}
-                                        variant='danger'
-                                        size='sm'
-                                    >
-                                        Remove Slot
-                                    </Button>
                                 </div>
-                        </Col>
-                    ))}
-                    </Row>
-                    <div className='d-flex justify-content-center gap-5 mt-2'>
-                        <Button onClick={addFullSlotHandler}>
-                            Add Full Slot
-                        </Button>
-                        <Button onClick={addHalfSlotsHandler}>
-                            Add Half Slots
-                        </Button>
-                    </div>
-                    <div className='d-flex justify-content-center align-items-center mt-4 gap-3'>
-                        <input
-                            type='text'
-                            value={articleTitle} 
-                            onChange={(e) => setArticleTitle(e.target.value)}
-                            placeholder='Article Title'
-                            className='form-control w-50'
-                        />
-                        <Button 
-                            onClick={publishArticleHandler}
-                            variant='success' 
-                            size='lg'
-                        >
-                            Publish Article
-                        </Button>
-                    </div>
+                                {
+                                    rows.map((row) => 
+                                    (
+                                        <Row key={row.id}>
+                                            {
+                                                row.columns.map((col) => 
+                                                (
+                                                    <Col 
+                                                        key={col.id}
+                                                        md={12 / row.columns.length}
+                                                        className='mb-2'
+                                                    >
+                                                        <DropBox id={col.id}>
+                                                            <RenderBlock
+                                                                block={col.block} 
+                                                                onContentChange={(content) => updateContent(col.id, content)}
+                                                            />
+                                                        </DropBox>
+                                                    </Col>
+                                                ))
+                                            }
+                                            <div className='text-center mb-4'>
+                                                <Button
+                                                    onClick={() => removeRow(row.id)}
+                                                    variant='danger'
+                                                    size='sm'
+                                                >
+                                                    Remove Row
+                                                </Button>
+                                            </div>
+                                        </Row>
+                                    ))
+                                }
+                            </Row>
+                            <div className='d-flex justify-content-center gap-5 mt-2'>
+                                <Button onClick={addFullRow}>
+                                    Add Whole Slot
+                                </Button>
+                                <Button onClick={addHalfRow}>
+                                    Add Half Slots
+                                </Button>
+                            </div>
+                            <div className='text-center mt-4'>
+                                <Button 
+                                    onClick={publishArticle}
+                                    variant='success' 
+                                    size='lg'
+                                >
+                                    Publish Article
+                                </Button>
+                            </div>
+                        </>
+                    }
                 </Col>
             </Row>
         </DragDropProvider>
