2de052e0e7a4037d8fc8b17b5bca997b9d466371
AntDBuck <antonybuck@outlook.com>
2026-04-06 17:09:57 +0100

    NoPage creation and Image.

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,7 +1,14 @@
+import dinoImg from '../assets/no-page-dino.png'
+
 function NoPage()
 {
     return (
-        <div></div>
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+            <img src={dinoImg} alt='Cartoon T-rex' className='w-25 mt-4' />
+            <h2>Oh No!</h2>
+            <h3>The page you searched for does not exist.</h3>
+            <h3>On the brightside, you got to see this cool dinosaur.</h3>
+        </div>
     );
 };
 
