f78499a3f5fb58e6094055b804e30a1de39808da
AntDBuck <antonybuck@outlook.com>
2026-04-06 17:15:04 +0100

    Completely changed old component to suit instructions for new app

--- frontend/src/components/NotConnected.jsx
+++ frontend/src/components/NotConnected.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
-
 export default function NotConnected() {
     return (
-        <div className="container text-center bg-light my-5" style={{ borderRadius: 20 }}>
-            <h1 className="mt-3 text-primary">Please Connect to Ropsten test network</h1>
-            <a href="https://faucet.ropsten.be/" rel="noopener noreferrer" target="_blank" className="btn btn-success">Get free Ether for Ropsten test network</a>
-            <h4 className="my-3">Follow these instructions for connect metamask with Ropsten test network</h4>
+        <div 
+            className="container bg-dark mt-5 py-3 text-light"
+            style={{ borderRadius: 20, minHeight: '85vh' }}
+        >
+            <h1 className="text-primary my-4 text-center">Please Connect Using Ganache And MetaMask</h1>
+            <h4 className="text-light text-center">
+                Follow the below instructions to connect MetaMask with a local Ganache network:
+            </h4>
             <hr />
-
-            <h6>1. Install  <a href="https://metamask.io/download.html" rel="noopener noreferrer" target="_blank" className="btn btn-outline-primary">Meta-mask</a></h6>
-            <h6>2. Create Your Account</h6>
-            <h6>3. Connect with Ropsten test network</h6>
-            <h6>4. Get Some Free Ether by Ropsten Faucet</h6>
+            <div className='d-flex flex-column align-items-center'>
+                <ol className='fs-4'>
+                    <li className='mt-3'>
+                        Install 
+                        <a 
+                            href="https://metamask.io/download.html" 
+                            rel="noopener noreferrer" 
+                            target="_blank" 
+                            className="btn btn-outline-light ms-2"
+                        >
+                            Meta-Mask
+                        </a>
+                    </li>
+                    <li className='mt-3'>Create a MetaMask account and sign in.</li>
+                    <li className='mt-3'>
+                        Install 
+                        <a 
+                            href="https://archive.trufflesuite.com/ganache/" 
+                            rel="noopener noreferrer" 
+                            target="_blank" 
+                            className="btn btn-outline-light ms-2"
+                        >
+                            Ganache
+                        </a>
+                    </li>
+                    <li className='mt-3'>Run Ganache and create a Ganache test network in MetaMask.</li>
+                    <li className='mt-3'>Import a Ganache account into your MetaMask.</li>
+                    <li className='mt-3'>Connect to the Dapp.</li>
+                </ol>
+            </div>
             <hr />
-            <img className="img-fluid h-50" src="img/ropston.jpg" alt="" />
-
         </div>
-    )
-}
+    );
+};
